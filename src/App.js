c0b9196e15f188ebfbfb2f5b03519d75bc619f99
LorenzoUliano <blazeylost@outlook.com>
2023-10-06 00:09:41 -0300

    arrumando coisas doidas

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+import ScreenStyle from './Styles/ScreenStyle'
+
+
+const Formulario = () => {
+    return (
+        <SafeAreaView>
+            <Text style={ScreenStyle.estilo}>Hello, Worsssld! lorem ipsum</Text>
+        </SafeAreaView>
+    );
+}
+
+export default Formulario;
