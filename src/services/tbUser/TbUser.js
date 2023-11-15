a0bdd1266956f3546fa60d964516264220c70e54
CarlossDMC <carlosdaniel.mc@hotmail.com>
2023-11-15 18:09:08 -0300

    Retirando coisas inuteis.

--- src/services/tbUser/TbUser.js
+++ src/services/tbUser/TbUser.js
@@ -1,25 +1,12 @@
 import db from "../sqlLite/DbManager";
 
-/**
- * INICIALIZAÇÃO DA TABELA
- * - Executa sempre, mas só cria a tabela caso não exista (primeira execução)
- */
 db.transaction((tx) => {
-  //<<<<<<<<<<<<<<<<<<<<<<<< USE ISSO APENAS DURANTE OS TESTES!!! >>>>>>>>>>>>>>>>>>>>>>>
-  //<<<<<<<<<<<<<<<<<<<<<<<< USE ISSO APENAS DURANTE OS TESTES!!! >>>>>>>>>>>>>>>>>>>>>>>
 
   tx.executeSql(
     "CREATE TABLE IF NOT EXISTS tb_user (bd_key INTEGER PRIMARY KEY, user_id, user_name VARCHAR(255), user_email VARCHAR(255), user_session TEXT, user_birthday DATE, user_daily_progress FLOAT, user_profession VARCHAR(255), user_weekly_progress FLOAT, user_weight FLOAT);"
   );
 });
 
-/**
- * CRIAÇÃO DE UM NOVO REGISTRO
- * - Recebe um objeto;
- * - Retorna uma Promise:
- *  - O resultado da Promise é o ID do registro (criado por AUTOINCREMENT)
- *  - Pode retornar erro (reject) caso exista erro no SQL ou nos parâmetros.
- */
 const create_user = (obj) => {
   return new Promise((resolve, reject) => {
     db.transaction((tx) => {
@@ -72,13 +59,6 @@ const executar = (sql, params = []) => {
   });
 };
 
-/**
- * ATUALIZA UM REGISTRO JÁ EXISTENTE
- * - Recebe o ID do registro e um OBJETO com valores atualizados;
- * - Retorna uma Promise:
- *  - O resultado da Promise é a quantidade de registros atualizados;
- *  - Pode retornar erro (reject) caso o ID não exista ou então caso ocorra erro no SQL.
- */
 const update_progress = (obj) => {
   return new Promise((resolve, reject) => {
     db.transaction((tx) => {
@@ -100,13 +80,6 @@ const update_progress = (obj) => {
   });
 };
 
-/**
- * BUSCA UM REGISTRO POR MEIO DO ID
- * - Recebe o ID do registro;
- * - Retorna uma Promise:
- *  - O resultado da Promise é o objeto (caso exista);
- *  - Pode retornar erro (reject) caso o ID não exista ou então caso ocorra erro no SQL.
- */
 const findAll = () => {
   return new Promise((resolve, reject) => {
     db.transaction((tx) => {
